116e022d5de07063c95c505b4de7b268e6d5ccec
Kwaku Owusu-Ansa <kwaku315@gmail.com>
2022-10-25 11:10:37 +0000

    initail commit

---
+++ lib/components/chefio_textfield.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChefioTextField extends StatelessWidget {
+  const ChefioTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
